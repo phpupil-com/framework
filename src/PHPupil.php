e14f0b8dd63a822072ca2e6b996ea133343d7c76
itophp <itophp@itophp.com>
2018-04-07 02:19:34 +0800

    v0.0.1
    提交启动文件

---
+++ src/PHPupil.php
@@ -0,0 +1,19 @@
+<?php
+namespace PHPupil\Framework;
+/**
+ * PHPupil框架启动文件
+ * @copyright JungoPhpFramework 深圳俊网网络有限公司 http://www.junnet.net/
+ * @author 吴跃忠 <357397264@qq.com>
+ */
+class PHPupil
+{
+
+    /**
+     * PHPupil constructor.
+     */
+    public function __construct()
+    {
+        // TODO
+    }
+
+}
